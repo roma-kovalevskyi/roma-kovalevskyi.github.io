903b09a0ce3436d65bf681e433fe6832f5030a0d
Roman <roman.bazu.2021@gmail.com>
2024-07-04 17:45:06 +0300

    Change slider to JS. Some markup fixes

--- og/js/main.js
+++ og/js/main.js
@@ -16,9 +16,42 @@
 //     console.log('Input event');
 // }, 300));
 
+// let clickCount = 0;
 
-document.querySelectorAll('.btn').forEach(function (btn) {
-    btn.addEventListener('click', function () {
-        alert('Click event');
+
+$(document).ready(function () {
+    let sliderPerRow = 5;
+    let slider = $('.main-slider__inner');
+
+    $('.main-slider__pagination-item').on('click', function() {
+        let index = $(this).index();
+
+        let marginLeft = -index * 100;
+        slider.css('margin-left', `${marginLeft}%`);
+        $(this).addClass('active').siblings().removeClass('active');
+    });
+
+    $('.movies_slider').each(function(index, element) {
+        let marginLeft = 0;
+        let sliderContent = $(element).find('.slider-content');
+
+        let sliderItems = sliderContent.children();
+        let sliderItemsCount = sliderItems.length;
+        let sliderMaxMovesCount = sliderItemsCount / sliderPerRow - 1;
+
+        $(element).find('.prev_btn').on('click', function() {
+            if (marginLeft === 0) return;
+
+            marginLeft += 100;
+            sliderContent.css('margin-left', `${marginLeft}%`);
+        });
+
+        $(element).find('.next_btn').on('click', function() {
+            if (marginLeft === -(sliderMaxMovesCount * 100)) return;
+
+            marginLeft -= 100;
+            sliderContent.css('margin-left', `${marginLeft}%`);
+        });
+
     });
 });
